b9994b0c4b100c8b10e61928ee0d4e3912dae366
noc801 <124569147+noc801@users.noreply.github.com>
2023-11-23 23:56:12 -0600

    Update Pay_Calculations.py

--- Pay_Calculations.py
+++ Pay_Calculations.py
@@ -1,30 +1,102 @@
-import random
-import math
+from tkinter import *
+from tkinter import messagebox
 
-lower = int(input("Enter low end: "))
-upper = int(input("Enter high end: "))
+def reset_entry():
+    hours_tf.delete(0, "end")
+    rate_tf.delete(0, "end")
 
-x = random.randint(lower, upper)
-print("\nYou only have ",
-      round(math.log(upper - lower + 1, 2)),
-      " chances to guess\n")
+def calculate_pay():
+    try:
+        x = float(hours_tf.get())
+        y = float(rate_tf.get())
+    except ValueError:
+        messagebox.showerror("Error", "Please enter valid numeric values.")
+        return
 
-count = 0
+    if x > 40:
+        reg = x * y
+        ot = (x - 40.0) * (y * 1.5)
+        z = reg + ot
+        tax = round(z * 0.1, 2)
+        after_tax = z - tax
+    else:
+        z = x * y
+        tax = round(z * 0.1, 2)
+        after_tax = z - tax
+    
+    message = (
+        f"Pay Before Tax: {z}\n"
+        f"Tax: {tax}\n"
+        f"Pay After Tax: {after_tax}"
+    )
 
-while count < math.log(upper - lower + 1, 2):
-    count += 1
+    messagebox.showinfo("Pay Information", message)
 
-    guess = int(input("Guess a number: "))
+ws = Tk()
+ws.title("Pay Calculator")
+ws.geometry("400x300")
+ws.config(bg="#68be70")
 
-    if x == guess:
-        print("Great job! You guessed correctly in ", count, " tries")
+var = IntVar()
 
-        break
-    elif x > guess:
-        print("Higher")
-    elif x < guess:
-        print("Lower")
+frame = Frame(
+    ws,
+    padx = 10,
+    pady = 10
+)
 
-if count >= math.log(upper - lower + 1, 2):
-    print("\nThe number is %d" % x)
-    print("\tSorry, you lost")
+frame.pack(expand = True)
+
+hours_lb = Label(
+    frame,
+    text="Enter Hours Worked"
+)
+
+hours_lb.grid(row = 1, column = 1)
+
+hours_tf = Entry(
+    frame,
+)
+
+hours_tf.grid(row = 1, column = 2, pady = 5)
+
+rate_lb = Label(
+    frame,
+    text = "Enter Hourly Rate",
+)
+
+rate_lb.grid(row = 2, column = 1)
+
+rate_tf = Entry(
+    frame,
+)
+
+rate_tf.grid(row = 2, column = 2, pady = 5)
+
+frame2 = Frame(
+    frame
+)
+frame2.grid(row = 3, column = 3, pady = 10)
+
+cal_rate = Button(
+    frame2,
+    text = "Calculate",
+    command = calculate_pay
+)
+cal_rate.pack(side=LEFT)
+
+reset_btn = Button(
+    frame2,
+    text = "Reset",
+    command = reset_entry
+)
+reset_btn.pack(side = LEFT)
+
+exit_btn = Button(
+    frame2,
+    text = "Exit",
+    command = lambda: ws.destroy()
+)
+exit_btn.pack(side = RIGHT)
+
+ws.mainloop()
